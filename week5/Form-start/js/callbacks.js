f83eeffc86750dcb9bd371dd11d8bc2d5083100a
MaxTidwell <mtidwell@byupathway.edu>
2025-12-02 22:01:48 -0700

    learning activity w5

---
+++ week5/Form-start/js/callbacks.js
@@ -0,0 +1,29 @@
+function calculate(a, b, callback) {
+  callback(a + b);
+}
+
+function displayResult(result) {
+  console.log('The result is: ' + result);
+}
+
+/* calculate(2, 3, displayResult) will use whatever function (displayResult) in the 3rd slot with a and b as the first 2 slots */
+
+
+
+/* simulated example */
+
+function fetchData(callback) {
+  // using setTimeout to simulate fetching data from a server
+  setTimeout(() => {
+    // This calls the 'callback' function and passes data to it.
+    callback('Data has been fetched');
+  }, 2000); // This simulates a 2-second delay from a service.
+}
+
+// function that processes the data
+function processData(data) {
+  console.log("Data received:", data);
+}
+
+// Call the fetchData function and pass the processData function as an argument.
+fetchData(processData);
